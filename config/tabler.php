67981a8af0d344344daeb511108150a370cc729b
Nikolas Evers <vintagesucks@users.noreply.github.com>
2019-02-02 15:32:33 +0100

    init testing (#8)

---
+++ config/tabler.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'suffix' => 'Kok',
+    'logo' => 'https://preview.tabler.io/demo/brand/tabler.svg',
+    'urls' => [
+        'logout' => 'logout',
+        'settings' => 'settings',
+        'searchUrl' => 'search',
+        'homepage' => '/',
+        'login' => 'login',
+        'post-login' => 'login',
+        'forgot' => 'password/reset',
+        'register' => 'register',
+        'post-register' => 'register',
+        'post-email' => 'password/email',
+        'post-reset' => 'password/reset'
+    ],
+    'footer' => null,
+    'support' => [
+        'search' => true,
+        'footer' => false,
+        'footer-menu' => false,
+    ]
+];
