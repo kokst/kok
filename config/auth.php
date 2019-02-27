e83a15c80c3c693b0f506eec605d4999cd3057d0
Nikolas Evers <vintagesucks@users.noreply.github.com>
2019-02-28 00:56:56 +0100

    namespace fix (#38)

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => Kokst\Core\Http\User::class,
         ],
 
         // 'users' => [
