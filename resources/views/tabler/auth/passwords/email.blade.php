ad071a982a1f95f5d20c446876df3730d1186230
Nikolas Evers <vintagesucks@users.noreply.github.com>
2019-03-02 15:01:28 +0100

    reset email

--- resources/views/tabler/auth/passwords/email.blade.php
+++ resources/views/tabler/auth/passwords/email.blade.php
@@ -3,10 +3,24 @@
     {!! Form::open(['url' => url(config('tabler.url.post-email', 'password/email')), 'method' => 'POST', 'class' => 'card']) !!}
     <div class="card-body p-6">
         <div class="card-title">@lang('email.title')</div>
+
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+            </div>
+        @endif
+
         <div class="form-group">
             {!! Form::label('email', trans('email.email'), ['class' => 'form-label']) !!}
-            {!! Form::email('email', old('email'), ['placeholder' => trans('email.email-placeholder'), 'class' => 'form-control']) !!}
+            {!! Form::email('email', old('email'), ['placeholder' => trans('email.email-placeholder'), 'class' => ['form-control', $errors->has('email') ? 'is-invalid' : '']]) !!}
+
+            @if ($errors->has('email'))
+                <span class="invalid-feedback">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+            @endif
         </div>
+
         <div class="form-footer">
             <button type="submit" class="btn btn-primary btn-block">@lang('email.send')</button>
         </div>
