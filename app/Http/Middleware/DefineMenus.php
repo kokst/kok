f3f3e5652f9770405f7d0c6a5850a9bb68d9e707
Nikolas Evers <vintagesucks@users.noreply.github.com>
2019-03-02 15:00:14 +0100

    reimplement menu

---
+++ app/Http/Middleware/DefineMenus.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Lavary\Menu\Facade as Menu;
+
+class DefineMenus
+{
+    public function handle($request, Closure $next)
+    {      
+        Menu::make('primary', function ($menu) {
+            $menu->add('Home', 'home');
+        });
+
+        return $next($request);
+    }
+}
