368723e4a3e4b5c46b0562d4ae806449627e1eaf
Nikolas Evers <vintagesucks@users.noreply.github.com>
2019-04-01 17:58:46 +0200

    codestyle

--- app/Http/Middleware/DefineMenus.php
+++ app/Http/Middleware/DefineMenus.php
@@ -10,9 +10,7 @@ class DefineMenus
     public function handle($request, Closure $next)
     {
         Menu::make('primary', function ($menu) {
-
             // $menu->add('Title', 'route');
-
         });
 
         return $next($request);
