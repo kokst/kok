0080fefa680aae1ec5bf669de484cfebf87d63b9
Nikolas Evers <vintagesucks@users.noreply.github.com>
2019-04-01 11:07:40 +0200

    Configurable enforcement of HTTPS

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Lavary\Menu\ServiceProvider::class,
+        Ecrmnn\LaravelHttps\Providers\ServiceProvider::class,
 
         /*
          * Application Service Providers...
