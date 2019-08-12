d74b9ec3461c92e2213e9fe0ca0610b4856e2fe0
Nikolas Evers <vintagesucks@users.noreply.github.com>
2019-08-13 00:26:48 +0200

    add laracasts/flash (#214)
    https://github.com/laracasts/flash

--- resources/views/tabler/layouts/main.blade.php
+++ resources/views/tabler/layouts/main.blade.php
@@ -88,6 +88,8 @@
                             <div class="col-md-9">
                     @endif
 
+                    @include('flash::message')
+
                     @yield('content')
 
                     @if($sidebar)
