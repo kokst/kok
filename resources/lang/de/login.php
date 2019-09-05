3a802a72ba92bbb7ffc4727cf1273b854e625bbf
Nikolas Evers <vintagesucks@users.noreply.github.com>
2019-09-05 10:55:52 +0200

    missing translations for auth flow (#239)

---
+++ resources/lang/de/login.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'title' => 'Füllen Sie alle Felder aus, um fortzufahren',
+    'email' => 'E-Mail',
+    'email-placeholder' => 'username@domain.com',
+    'password' => 'Passwort',
+    'forgot' => 'Passwort vergessen?',
+    'password-placeholder' => '********',
+    'remeber-me' => 'Angemeldet bleiben',
+    'signin' => 'Anmelden',
+    'register' => 'Account erstellen',
+];
