c6638f80de2a9ccd58eb893c3bf78a419a55d630
Nikolas Evers <vintagesucks@users.noreply.github.com>
2019-09-05 10:51:09 +0200

    missing translations for auth flow

---
+++ resources/lang/de/email.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => 'Füllen Sie alle Felder aus, um fortzufahren',
+    'email' => 'E-Mail',
+    'email-placeholder' => 'username@domain.com',
+    'send' => 'Senden',
+];
